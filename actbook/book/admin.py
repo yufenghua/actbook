6d4917eec4da607cefcd6f48c41b192b844d3f79
yufenghua <panqiaolin198909@gmail.com>
2018-05-05 17:47:54 +0800

    代码提交，models建立

--- actbook/book/admin.py
+++ actbook/book/admin.py
@@ -3,4 +3,9 @@ from __future__ import unicode_literals
 
 from django.contrib import admin
 
-# Register your models here.
+from .models import *
+
+admin.site.register(BookType)
+admin.site.register(BookSubType)
+admin.site.register(BookItem)
+
--- actbook/book/models.py
+++ actbook/book/models.py
@@ -2,5 +2,41 @@
 from __future__ import unicode_literals
 
 from django.db import models
+from django.utils.encoding import python_2_unicode_compatible
+
+
+
 
 # Create your models here.
+@python_2_unicode_compatible
+class BookType(models.Model):
+	name=models.CharField(max_length=600)
+	def __str__(self):
+		return self.name
+@python_2_unicode_compatible
+class BookSubType(models.Model):
+	name=models.CharField(max_length=600)
+	type=models.ForeignKey(BookType, on_delete=models.CASCADE)
+	def __str__(self):
+		return self.name+self.type.name
+
+		
+
+# 时间 收入/支出 大类 小类 金额 备注
+@python_2_unicode_compatible
+class BookItem(models.Model):
+	inouttype = (
+        ('INPUT', '收入'),
+        ('OUTPUT', '支出'),
+    )
+	time=models.DateTimeField("time")
+	type=models.ForeignKey(BookType, on_delete=models.CASCADE)
+	subtype=models.ForeignKey(BookSubType, on_delete=models.CASCADE)
+	inout=models.CharField(max_length=8, choices=inouttype)
+	money=models.DecimalField(max_digits=8, decimal_places=2)
+	memo=models.CharField(max_length=256)
+	def __str__(self):
+		return self.memo
+
+
+
