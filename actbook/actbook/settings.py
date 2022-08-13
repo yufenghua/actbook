84eb51dad4093266d8be54770037c5136f4bdd11
qiaolin <yufenghuapan@163.com>
2022-08-13 20:46:15 +0800

    增加一个print

--- actbook/actbook/settings.py
+++ actbook/actbook/settings.py
@@ -118,5 +118,6 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
+print(PROJECT_ROOT)
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(PROJECT_ROOT, 'static')
